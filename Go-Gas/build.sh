01f7f2498f403ffbfba10f23b3fdf28f4916cf7c
Constantino Fernandez Traba <constantino.fernandez@mobgen.com>
2016-09-25 13:47:52 +0200

    better go mongo connection

---
+++ Go-Gas/build.sh
@@ -0,0 +1,3 @@
+cd src/benchmark/
+
+docker build -t my-golang-app .
--- Go-Gas/src/benchmark/main.go
+++ Go-Gas/src/benchmark/main.go
@@ -4,6 +4,7 @@ import (
   "github.com/go-gas/gas"
   "net/http"
   //"fmt"
+  "os"
   "runtime"
   "log"
   "strconv"
@@ -19,72 +20,58 @@ type Person struct {
 
 type PersonAll interface {}
 
-type PersonExtended struct {
-  _Id bson.ObjectId
-  Name string
-  Phone string 
-  Index float32
-  isActive bool
-  Guid string
-  Balance string
-  Picture string
-  Age float32
-  Eyecolor string
-  Gender string
-
-  Company string
-  Email string 
-  Address string
-  About string
-  Registered string
-  Latitude float32
-  Longitude float32
-  Tags []string
-  Friends []interface{}
-  Greeting string
-  FavoriteFruit string
+type DataStore struct {
+    session *mgo.Session
 }
+
+var (
+  masterDataStore = DataStore{GetDBSession()}
+)
 
 func main() {
   g := gas.Default()
   g.LoadConfig("config.yaml")
 
-  runtime.GOMAXPROCS(6)
+  runtime.GOMAXPROCS(4)
 
   g.Router.Get("/", Index)
-  g.Router.Get("/user", GetUser)
   g.Router.Post("/person", CreatePerson)
   g.Router.Get("/test", GetTestData)
 
-  g.Run("localhost:8181")
+  g.Run("0.0.0.0:8181")
 }
 
 func GetDBSession() *mgo.Session {
-  session, err := mgo.Dial("localhost:27017")
+
+  mongoURL := os.Getenv("MONGODB_URL")
+
+  if mongoURL == "" { mongoURL = "localhost:27017" }
+
+  session, err := mgo.Dial(mongoURL)
   if err != nil {
+    log.Fatal("error connecting mongodb:",err,mongoURL)    
     panic(err)
   }  
 
   // Optional. Switch the session to a monotonic behavior.
-  session.SetMode(mgo.Monotonic, true)  
+  //session.SetMode(mgo.Monotonic, true) 
+  session.SetMode(mgo.Strong, true) 
 
   return session
+}
+
+func GetDataStore() *DataStore {
+  return &DataStore{masterDataStore.session.Copy()}
+  //return &GetDBSession()
 }
 
 func Index(ctx *gas.Context) error {
   return ctx.HTML(http.StatusOK, "Micro service! <br> <a href=\"/user\">json response example</a>")
 }
 
-func GetUser(ctx *gas.Context) error {
-  return ctx.JSON(http.StatusOK, gas.H{
-    "name": "John",
-    "age":  32,
-  })
-}
-
 func GetTestData(ctx *gas.Context) error {
 
-  session := GetDBSession()
+  session := GetDataStore().session
   defer session.Close()
 
   c := session.DB("local").C("testData")
@@ -92,15 +79,13 @@ func GetTestData(ctx *gas.Context) error {
   limit := 100
 
   if i, err := strconv.Atoi(ctx.GetParam("limit")); err == nil {
-      //fmt.Printf("i=%d, type: %T\n", i, i)
       limit = i
   }  
 
   result := []PersonAll{}
-  //var interfaceSlice []interface{} = make([]interface{}, 100)
   err := c.Find(bson.M{}).Limit(limit).All(&result)
   if err != nil {
-    log.Fatal(err)
+    log.Fatal("error on mongodb find:",err)
   }
 
   //fmt.Println("result[0]:", result[0])  
@@ -111,7 +96,7 @@ func GetTestData(ctx *gas.Context) error {
 
 func CreatePerson(ctx *gas.Context) error {
 
-  session := GetDBSession()
+  session := GetDataStore().session
   defer session.Close()
 
   c := session.DB("test").C("people")
@@ -119,7 +104,7 @@ func CreatePerson(ctx *gas.Context) error {
   err := c.Insert(&Person{bson.NewObjectId(), "Ale", "+55 53 8116 9639"},
            &Person{bson.NewObjectId(), "Cla", "+55 53 8402 8510"})
   if err != nil {
-    log.Fatal(err)
+    log.Fatal("error on mongodb create:",err)
   }  
 
   return ctx.JSON(http.StatusOK, err)
